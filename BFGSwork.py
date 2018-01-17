64bda0beac5cb6ebfc6cf277dacabacef3e8e02c
CHUCKIEMAGS <35340575+CHUCKIEMAGS@users.noreply.github.com>
2018-01-17 15:39:37 -0600

    Add files via upload

---
+++ BFGSwork.py
@@ -0,0 +1,13 @@
+# Implementation of BFGS algorithm
+
+import numpy as np 
+
+X = np.array(([3,5], [5,1], [10,2]), dtype=float) # x data
+y = np.array(([75], [82], [93]), dtype=float) # y data
+degreeHyperparam = 3 # degrees of polynomial prediction
+invHessian = np.identity(degreeHyperparam + 1) # initialize inverse Hessian
+
+yhat = 60 * X # prediction initializer
+
+errorSum = np.sum(np.square(y - yhat))
+print(errorSum)
